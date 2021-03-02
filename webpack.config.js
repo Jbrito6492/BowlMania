dda9bf3d5018ea9ee8470da34a79ae72eaad5c32
Josh Brito <jbrito6492@gmail.com>
2021-03-02 14:15:39 -0700

    added base dependencies

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SRC_DIR = path.join(__dirname, 'src');
+const PUBLIC_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  mode: 'development',
+  target: 'web',
+  entry: {
+    app: `${SRC_DIR}/index.jsx`
+  },
+  output: {
+    path: PUBLIC_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: [/\.jsx$/],
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      },
+    },
+    {
+      test: /\.(css|less)$/,
+      exclude: /node_modules/,
+      use: [{ loader: 'style-loader' },
+      {
+        loader: 'css-loader',
+        options: {
+          importLoaders: 1,
+          modules: {
+            localIdentName: '[path][name]__[local]--[hash:base64:5]'
+          }
+        }
+      }
+      ]
+    }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Score Tracker',
+      template: `${SRC_DIR}/templates/index.ejs`,
+      favicon: `${PUBLIC_DIR}/favicon.ico`
+    })
+  ]
+};
