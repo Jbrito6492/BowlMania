8481c10838e1540af3211c611e8c63160cc238f3
Josh Brito <jbrito6492@gmail.com>
2021-03-05 20:37:50 -0700

    refactored ScoreBoard takeTurn logic

--- src/hooks/ScoreBoard.js
+++ src/hooks/ScoreBoard.js
@@ -1,55 +1,15 @@
 import React, { useState } from "react";
-import { isStrike, isSpare, isNewFrame, isFinalFrame, round10StrikeOrSpare, calculateCurrentFrame } from "../helpers";
+import { isStrike, isSpare, isNewFrame } from "../helpers";
 import CounterHook from "./Counter";
 
 export default function ScoreBoard() {
   const { counter, incrementCounter } = CounterHook();
   const [scoreboard, setScoreboard] = useState({
-    frames: [],
     pins: [],
     totalScore: [],
     remainingPins: 10,
     gameOver: false
   });
-
-  // const takeTurn = (value) => {
-  //   const { frames, pins } = scoreboard;
-  //   if (isFinalFrame(frames)) {
-  //     if (isStrike(value) && pins.length === 19) {
-  //       const currentThrow = pins.concat(value).slice(-3);
-  //       setScoreboard((prevState) => ({ ...prevState, frames: [...frames, currentThrow], pins: pins.concat(value), gameOver: true }));
-  //     }
-  //     // if (round10StrikeOrSpare(pins))) {
-  //     //   if (pins.length === 20) {
-  //     //     const currentThrow = pins.concat(value).slice(-3);
-  //     //     setScoreboard((prevState) => ({ ...prevState, frames: [...frames, currentThrow], pins: pins.concat(value), gameOver: true }))
-  //     //   } else {
-  //     //     setScoreboard((prevState) => ({ ...prevState, pins: pins.concat(value) }));
-  //     //   }
-  //     // } else {
-  //     //   if (pins.length === 19 || pins.length === 20) {
-  //     //     const currentThrow = pins.concat(value).slice(-2);
-  //     //     setScoreboard((prevState) => ({ ...prevState, frames: [...frames, currentThrow], pins: pins.concat(value), gameOver: true }))
-  //     //   } else {
-  //     //     setScoreboard((prevState) => ({ ...prevState, pins: pins.concat(value) }));
-  //     //   }
-  //     // }
-
-  //   } else {
-  //     if (isStrike(value)) {
-  //       setScoreboard((prevState) => ({ ...prevState, frames: [...frames, [value]], pins: pins.concat([value, null]) }));
-  //     } else {
-  //       const remainingPins = 10 - value;
-  //       if (isNewFrame(pins)) {
-  //         setScoreboard((prevState) => ({ ...prevState, pins: pins.concat(value), remainingPins }));
-  //       } else {
-  //         const currentThrow = pins.concat(value).slice(-2);
-  //         setScoreboard((prevState) => ({ ...prevState, frames: [...frames, currentThrow], pins: pins.concat(value), remainingPins: 10 }));
-  //       }
-  //     }
-  //   }
-  //   console.log(scoreboard)
-  // };
 
   const takeTurn = (roll) => {
     const { pins } = scoreboard;
@@ -65,9 +25,6 @@ export default function ScoreBoard() {
       }
       break;
     }
-    console.log(pins);
-    console.log("counter", counter);
-    console.log("current frame", calculateCurrentFrame(counter));
   }
 
   const getTotalScore = () => {
