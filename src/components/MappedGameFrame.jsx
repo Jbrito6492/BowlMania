b725741cb016c427ecb706e91f5a4bfc1059ebe0
Josh Brito <65372461+Jbrito6492@users.noreply.github.com>
2021-05-03 11:07:33 -0700

    Update MappedGameFrame.jsx

--- src/components/MappedGameFrame.jsx
+++ src/components/MappedGameFrame.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import GameFrame from "./GameFrame.jsx";
 import { calculateCurrentFrame, isNewFrame } from "../helpers";
 import uuid from "react-uuid";
